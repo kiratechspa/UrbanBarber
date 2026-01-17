75e12998d075354b063235dfa4deec3440148efa
kiratechspa <kiratechspa@gmail.com>
2026-01-17 15:24:10 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -80,3 +80,13 @@ window.onclick = function(event) {
         modal.style.display = 'none';
     }
 }
+
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
